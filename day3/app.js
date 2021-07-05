08c0c4dfafbca1ac80f9fee8519c3f9a475d867d
Alejandro Butron <alesvoy@gmail.com>
2021-07-05 13:11:52 -0500

    Day 3 Challenge Part 1 Solved

---
+++ day3/app.js
@@ -0,0 +1,12 @@
+// Part 1
+var str1 = "Welcome";
+var str2 = "Hello, World!";
+var str3 = "Disaster";
+
+var newStr1 = str1.slice(0, 3);
+var newStr2 = str2.slice(0, 6);
+var newStr3 = str3[0];
+
+console.log(newStr1);
+console.log(newStr2);
+console.log(newStr3);
