306afff7f985715da8971803bf9de2415de42a8a
Alejandro Butron <alesvoy@gmail.com>
2021-07-05 13:17:40 -0500

    Day 3 Challenge Part 2 Solved

--- day3/app.js
+++ day3/app.js
@@ -1,4 +1,4 @@
-// Part 1
+// Part 1 - Truncate
 var str1 = "Welcome";
 var str2 = "Hello, World!";
 var str3 = "Disaster";
@@ -10,3 +10,25 @@ var newStr3 = str3[0];
 console.log(newStr1);
 console.log(newStr2);
 console.log(newStr3);
+
+// Part 2 - Char Builder
+var person = "Mark";
+var car = "Jeep";
+var animal = "Leopard";
+var city = "Townsville";
+var game = "Cards";
+var water = "Flows";
+var clean = "Shower";
+var mood = "Frown";
+
+var strChallenge =
+  person[0] +
+  car[1] +
+  city[7] +
+  city[0].toLowerCase() +
+  game[3] +
+  water[2] +
+  water[3] +
+  city[3];
+
+console.log(strChallenge);
