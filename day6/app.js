daaf952bcf1527bfd32730bbeb858575f6916c31
Alejandro Butron <alesvoy@gmail.com>
2021-07-06 14:11:17 -0500

    Day 6 Challenge Part 1 Solved

--- day6/app.js
+++ day6/app.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+//Part 1 - Traffic
+var status = "Red";
+var emergencyVehicle = false;
+var isInIntersection = true;
+
+if (
+  (status === "Green" && emergencyVehicle === false) ||
+  isInIntersection === true
+) {
+  console.log("You can drive");
+} else if (status === "Yellow" && emergencyVehicle === false) {
+  console.log("Prepare to stop");
+} else if (status === "Red" || emergencyVehicle === true) {
+  console.log("Do not drive");
+}
+
+//Part 2 - Grades
+var grades = "AA";
